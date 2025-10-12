c79ebdb0b4962966e641408f9e41bdfe18e9d11e
kibria <mdkibria.dev@gmail.com>
2025-10-12 19:59:26 +0600

    fixed: uncommand g-capcha verificaiton validation from auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
         $credentials = $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
-            // 'g-recaptcha-response' => 'required|captcha',
+            'g-recaptcha-response' => 'required|captcha',
         ]);
 
         if (Auth::attempt(['email' => $credentials['email'], 'password' => $credentials['password']])) {
